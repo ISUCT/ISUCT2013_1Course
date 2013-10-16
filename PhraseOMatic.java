3ac7b730d8812a19dbe1e8e3c9163e214ee1f618
Your Name <you@example.com>
2013-10-16 10:54:25 +0400

    Dobavlen PhraseOMatic

--- PhraseOMatic.java
+++ PhraseOMatic.java
@@ -1,14 +1,14 @@
 public class PhraseOMatic {
-public static void main(Strin[] args) {
+public static void main(String[] args) {
 String[] wordListOne = {"выносливый", "эмоциональный", "экстремальный", "внимательный"};
 String[] wordListTwo = {"мудрый", "чуткий", "бесценный", "лучистый"};
 String[] wordListThree = {"друг", "человек", "подарок", "день"};
 int oneLenght = wordListOne.length;
 int twoLenght = wordListTwo.length;
 int threeLenght = wordListThree.length;
-int rand1 = (int) (Math.rendom() * oneLenght);
-int rand2 = (int) (Math.rendom() * twoLenght);
-int rand3 = (int) (Math.rendom() * threeLenght);
+int rand1 = (int) (Math.random() * oneLenght);
+int rand2 = (int) (Math.random() * twoLenght);
+int rand3 = (int) (Math.random() * threeLenght);
 String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
 System.out.println("Все, что нам нужно, - это " + phrase);
   }
