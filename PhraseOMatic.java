a60ce1437705bf4c82ef18492f6a0531fd013aa5
user <you@example.com>
2013-10-16 11:01:49 +0400

    Izmenenie generatora phrase

--- PhraseOMatic.java
+++ PhraseOMatic.java
@@ -1,15 +1,15 @@
 public class PhraseOMatic {
 public static void main (String[] args) {
-String[] wordListOne = ("умный", "известный", "интересный");
-String[] wordListTwo = ("личностный", "фирменный", "правдивый");
-String[] wordListThree = ("код", "знак", "образец");
+String[] wordListOne = {"умный", "известный", "интересный"};
+String[] wordListTwo = {"личностный", "фирменный", "правдивый"};
+String[] wordListThree = {"код", "знак", "образец"};
 int oneLength = wordListOne.length;
-int oneLength = wordListTwo.length;
-int oneLength = wordListThree.length;
-int rand1 = (int) (Math.random() * oneLenght);
-int rand2 = (int) (Math.random() * twoLenght);
-int rand3 = (int) (Math.random() * threeLenght);
-String phrase = wordListOne[rand1] + " " + worldListTwo[rand2] + " " + worldListTwo[rand3];
+int twoLength = wordListTwo.length;
+int threeLength = wordListThree.length;
+int rand1 = (int) (Math.random() * oneLength);
+int rand2 = (int) (Math.random() * twoLength);
+int rand3 = (int) (Math.random() * threeLength);
+String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListTwo[rand3];
 System.out.println("Все, что нам нужно, - это " + phrase);
 }
 }
