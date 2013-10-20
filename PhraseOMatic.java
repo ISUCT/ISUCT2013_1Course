c32130934d218859cba1a64ad43753f5a6f807b2
GoldSky37 <1-2-93@bk.ru>
2013-10-20 08:16:46 -0700

    PhraseOMatic.java

---
+++ PhraseOMatic.java
@@ -0,0 +1,16 @@
+public class PhraseOMatic {
+public static void main (String[] args) {
+String[] wordListOne = {"êðóãëîñóòî÷íûé", "ïîëîæèòåëüíûé", "ãîòîâûé", "÷èñëîâîé", "îáùèé", "ñóùåñòâóþùèé", "ïðàâèëüíûé"};
+String[] wordListTwo = {"êðàéíèé", "ðàáîòàþùèé", "èñïîëüçîâàííûé", "íàïðàâëåííûé", "äèíàìè÷íûé", "ãîäîâîé", "äíåâíîé"};
+String[] wordListThree = {"îò÷åò", "ïðîöåññ", "òèï", "ïîäõîä", "óðîâåíü", "ìåòîä", "âèä", "ïåðèîä", "âûâîä", "êîò"};
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
+ 
+String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+System.out.println("Âñå, ÷òî íàì íóæíî, - ýòî " + phrase);
+        }
+}
