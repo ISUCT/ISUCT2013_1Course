5aa28bbdf3002f30f64c813383ee5bbf209ccd9c
AlexCPU7 <AlexCPU7@mail.ru>
2013-10-20 22:29:40 +0400

    Update WorldHello.java

--- WorldHello.java
+++ WorldHello.java
@@ -1,6 +1,6 @@
 public class WorldHello{
     public static void main (String[] args){
-        System.out.println("I lick Git");
-        System.out.println("and Java");
+        System.out.println("Semenov Alexey");
+        System.out.println("Sergeevich");
     }
 }
