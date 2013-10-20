785fb6addb020f79e0f8fcb031f212669a3c9644
GoldSky37 <1-2-93@bk.ru>
2013-10-20 09:13:08 -0700

    Update PhraseOMatic.java

--- PhraseOMatic.java
+++ PhraseOMatic.java
@@ -4,15 +4,19 @@ String[] wordListOne = {"Russia", "Germany", "French", "Brazil", "USA", "Serbia"
 String[] wordListTwo = {"Polland", "Portugal", "Cuba", "Austria", "Hungary", "Monako", "Horvatia"};
 String[] wordListThree = { "England", "Eesti", "Schweiz", "Chehia", "Makedonia", "Mongolia", "Turcia", "Andorra", "Italia"};
 String[] wordListFour = { "Litva", "Latvia", "Ukraina", "Kipr", "Irlandia"};
+String[] wordListFive = { "Gruzia", "Armenia", "Belorussia", "Shotlandia", "Grenlandia"};        
         int oneLength = wordListOne.length;
         int twoLength = wordListTwo.length;
         int threeLength = wordListThree.length;
         int fourLength = wordListFour.length;
+        int fiveLength = wordListFive.length;
         int rand1 = (int) (Math.random() * oneLength);
         int rand2 = (int) (Math.random() * twoLength);
         int rand3 = (int) (Math.random() * threeLength);
         int rand4 = (int) (Math.random() * fourLength);
+        int rand5 = (int) (Math.random() * fiveLength);
  String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3] + " " + wordListFour[rand4];
-System.out.println("Âñå, ÷òî íàì íóæíî, - ýòî " + phrase);
++ " " + wordListFive[rand5];
+System.out.println("Ugadaj kakaia strana poiavitsa ? " + phrase);
         }
 }
