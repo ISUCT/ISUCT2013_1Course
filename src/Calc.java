43e9cd918fb24d7265eb3a245657ecf14c03d33b
Tanya95 <tanya-cat95@mail.ru>
2013-10-30 16:08:21 +0400

    Calculator1

--- src/Calc.java
+++ src/Calc.java
@@ -5,46 +5,59 @@
 
 /**
  *
- * @author Stud_17
+ * @author stud_17
  */
 public class Calc {
-    public void taskA(){
-        //      корень кубический(sin(x)+log2(a+x))
-        // y = -------------------------------------
-        //       модуль((x+b))
-        // a=5, b=8 x1 = pi, x2 = pi+0.5pi, x3 = pi+1pi, x4=pi+1.5pi 
-        int a = 5;
-        int b=8;
-        
-        float chisl;
+    void simple(double b,float x){
+         float chisl;
         float znamen;
-        
-        float x=(float)Math.PI;
-        chisl = (float)Math.pow((Math.sin(x)+(Math.log(a+x)/Math.log(2))),(1/3.0));
-        znamen = (float) Math.abs((x+b)*Math.pow(10,-3));
-        float y = chisl/znamen;
-        System.out.println("x="+x+"; "+"y="+y);
-        
-        x=(float)(Math.PI+0.5*Math.PI);
-        chisl = (float)Math.pow((Math.sin(x)+(Math.log(a+x)/Math.log(2))),(1/3.0));
-        znamen = (float) Math.abs((x+b)*Math.pow(10,-3));
-        y = chisl/znamen;
-        System.out.println("x="+x+"; "+"y="+y);
-        
-        x=(float)(Math.PI+1*Math.PI);
-        chisl = (float)Math.pow((Math.sin(x)+(Math.log(a+x)/Math.log(2))),(1/3.0));
-        znamen = (float) Math.abs((x+b)*Math.pow(10,-3));
-        y = chisl/znamen;
-        System.out.println("x="+x+"; "+"y="+y);
-        
-        x=(float)(Math.PI+1.5*Math.PI);
-        chisl = (float)Math.pow((Math.sin(x)+(Math.log(a+x)/Math.log(2))),(1/3.0));
-        znamen = (float) Math.abs((x+b)*Math.pow(10,-3));
-        y = chisl/znamen;
-        System.out.println("x="+x+"; "+"y="+y);
-        
+        chisl = (float) (1 + Math.pow(Math.sin(Math.pow(b, 3) + Math.pow(x, 3)), 2));
+        znamen = (float) (Math.pow((Math.pow(b, 3) + Math.pow(x, 3)), 1 / 3.0));
+        float y = chisl / znamen;
+        System.out.println("x=" + x + ";" + "y=" + y);
     }
-    public static void main(String[] args){
+
+    public void taskA() {
+        double b = 2.5;
+        float x = (float) 1.1;
+        simple(b,x);
+//        float chisl;
+//        float znamen;
+//        chisl = (float) (1 + Math.pow(Math.sin(Math.pow(b, 3) + Math.pow(x, 3)), 2));
+//        znamen = (float) (Math.pow((Math.pow(b, 3) + Math.pow(x, 3)), 1 / 3.0));
+//        float y = chisl / znamen;
+//        System.out.println("x=" + x + ";" + "y=" + y);
+//
+        x = (float) 2.4;
+        simple(b,x);
+//        chisl = (float) (1 + Math.pow(Math.sin(Math.pow(b, 3) + Math.pow(x, 3)), 2));
+//        znamen = (float) (Math.pow((Math.pow(b, 3) + Math.pow(x, 3)), 1 / 3.0));
+//        y = chisl / znamen;
+//        System.out.println("x=" + x + ";" + "y=" + y);
+//        
+        x = (float) 3.6;
+        simple(b,x);
+//        chisl = (float) (1 + Math.pow(Math.sin(Math.pow(b, 3) + Math.pow(x, 3)), 2));
+//        znamen = (float) (Math.pow((Math.pow(b, 3) + Math.pow(x, 3)), 1 / 3.0));
+//        y = chisl / znamen;
+//        System.out.println("x=" + x + ";" + "y=" + y);
+//        
+        x = (float) 1.7;
+        simple(b,x);
+//        chisl = (float) (1 + Math.pow(Math.sin(Math.pow(b, 3) + Math.pow(x, 3)), 2));
+//        znamen = (float) (Math.pow((Math.pow(b, 3) + Math.pow(x, 3)), 1 / 3.0));
+//        y = chisl / znamen;
+//        System.out.println("x=" + x + ";" + "y=" + y);
+//        
+        x = (float) 3.9;
+        simple(b,x);
+//        chisl = (float) (1 + Math.pow(Math.sin(Math.pow(b, 3) + Math.pow(x, 3)), 2));
+//        znamen = (float) (Math.pow((Math.pow(b, 3) + Math.pow(x, 3)), 1 / 3.0));
+//        y = chisl / znamen;
+//        System.out.println("x=" + x + ";" + "y=" + y);
+    }
+
+    public static void main(String[] args) {
         Calc my = new Calc();
         my.taskA();
     }
