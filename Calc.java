8324d6e9a94b72d0494cf7d0d79340090968b96f
Unutoriya <unutoriya@gmail.com>
2013-11-10 18:24:20 +0300

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -1,44 +1,54 @@
-public class Calc {
- public void taskA(){
-	 
-	 int a = 5;
-	 int b = 8;
-	 
-	 float chisl;
-	 float znam;
-	 
-	 float x=(float)0.1;
-	 chisl = (float) ((float)Math.log(a)/Math.log(10)*(Math.abs(Math.pow(b,2))-(Math.pow(x,2))));
-	 znam = (float) ((float)Math.pow((Math.abs((Math.pow(x,2))-((Math.pow(a,2))))),(1/5.0)));
-	 float y1 = chisl/znam;
-	 System.out.println("x="+x+"; "+"y="+y1);
-	 
-	 x=(float)0.3;
-	 chisl = (float) ((float)Math.log(a)/Math.log(10)*(Math.abs(Math.pow(b,2))-(Math.pow(x,2))));
-	 znam = (float) ((float)Math.pow((Math.abs((Math.pow(x,2))-((Math.pow(a,2))))),(1/5.0)));
-	 float y2 = chisl/znam;
-	 System.out.println("x="+x+"; "+"y="+y2);
-	 
-	 x=(float)0.4;
-	 chisl = (float) ((float)Math.log(a)/Math.log(10)*(Math.abs(Math.pow(b,2))-(Math.pow(x,2))));
-	 znam = (float) ((float)Math.pow((Math.abs((Math.pow(x,2))-((Math.pow(a,2))))),(1/5.0)));
-	 float y3 = chisl/znam;
-	 System.out.println("x="+x+"; "+"y="+y3);
-	 
-	 x=(float)0.45;
-	 chisl = (float) ((float)Math.log(a)/Math.log(10)*(Math.abs(Math.pow(b,2))-(Math.pow(x,2))));
-	 znam = (float) ((float)Math.pow((Math.abs((Math.pow(x,2))-((Math.pow(a,2))))),(1/5.0)));
-	 float y4 = chisl/znam;
-	 System.out.println("x="+x+"; "+"y="+y4);
-	 
-	 x=(float)0.65;
-	 chisl = (float) ((float)Math.log(a)/Math.log(10)*(Math.abs(Math.pow(b,2))-(Math.pow(x,2))));
-	 znam = (float) ((float)Math.pow((Math.abs((Math.pow(x,2))-((Math.pow(a,2))))),(1/5.0)));
-	 float y5 = chisl/znam;
-	 System.out.println("x="+x+"; "+"y="+y5);
+import java.util.ArrayList;
+
+public class Calc{
+    public float singleValue(int a,float b,float x){
+        float chisl;
+        float znam;
+        chisl = (float) ((float)Math.log(a)/Math.log(10)*(Math.abs(Math.pow(b,2))-(Math.pow(x,2))));
+         znam = (float) ((float)Math.pow((Math.abs((Math.pow(x,2))-((Math.pow(a,2))))),(1/5.0)));
+         float y = chisl/znam;
+         System.out.println("x="+x+"; "+"y="+y);
+         return y;
+    }
+      public ArrayList<Float> taskB(){
+        ArrayList<Float> y= new ArrayList<Float>();
+         
+         int a = 2;
+         float b = 1.1f;
+        
+         float x=(float)0.1;
+         y.add(singleValue(a, b, x));
+         
+         x=(float)0.3;
+         y.add(singleValue(a, b, x));
+         
+         x=(float)0.4;
+         y.add(singleValue(a, b, x));
+         
+         x=(float)0.45;
+         y.add(singleValue(a, b, x));
+         
+         x=(float)0.65;
+         y.add(singleValue(a, b, x));
+         
+         return y;
+ }
+         
+         public void taskA(int a, float b){
+             float xn = (float)0.08;
+             float xk = (float)1.08;
+             float dx = (float)0.2;
+             for (float x = xn;x<=xk;x=x+dx){
+                 float result = singleValue(a, b, x);
+                 System.out.println("x="+x+"; "+"y="+result);
+         }
+         
  }
     public static void main(String[] args){
         Calc my = new Calc();
-        my.taskA();
+        ArrayList<Float> res=my.taskB();
+        for(int i=0;i<res.size();i++){
+            System.out.println("result >"+res.get(i));
+    }
     }
 }
