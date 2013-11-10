cb1b53b4bc4e44f65e51087428bed3c14fec1b16
Your Name <you@example.com>
2013-11-10 22:21:23 +0300

    Calc

--- MyProject/src/Calc.java
+++ MyProject/src/Calc.java
@@ -1,9 +1,7 @@
-
 import java.util.ArrayList;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 
@@ -12,55 +10,83 @@ import java.util.ArrayList;
  * @author Ruler
  */
 public class Calc {
-    public void taskB() {
-        int a = 2 ;
-        double b = 4.1;
-        
+    public float singleValue(float a,float b,float x){
         float chisl;
         float znamen;
-        
-        float x1=(float)1.24;
-        chisl = (float) Math.log((Math.pow(b,2))-(Math.pow(x1,2)));
-        znamen = (float)Math.pow((Math.abs(Math.pow(x1,2))-(Math.pow(a,2))),(1/3.0));
+        chisl = (float) Math.log((Math.pow(b,2))-(Math.pow(x,2)));
+        znamen = (float)Math.pow((Math.abs(Math.pow(x,2))-(Math.pow(a,2))),(1/3.0));
         float y = chisl/znamen;
-        System.out.println("x="+x1+"; "+"y="+y);
+        System.out.println("x="+x+"; "+"y="+y);
+        return y;
+    }
+    
+       public ArrayList<Float> taskB(){
+          ArrayList<Float> y = new ArrayList<Float>();
         
-        float x2 = (float)1.38;
-        chisl = (float) Math.log((Math.pow(b,2))-(Math.pow(x2,2)));
-        znamen = (float)Math.pow((Math.abs(Math.pow(x2,2))-(Math.pow(a,2))),(1/3.0));
-        y = chisl/znamen;
-        System.out.println("x="+x2+"; "+"y="+y);
+        float a = 2;
+        float b = 4.1f;
+        float x = (float)1.24;
+        y.add(singleValue(a, b, x));
         
-        float x3 = (float)2.38;
-        chisl = (float) Math.log((Math.pow(b,2))-(Math.pow(x3,2)));
-        znamen = (float)Math.pow((Math.abs(Math.pow(x3,2))-(Math.pow(a,2))),(1/3.0));
-        y = chisl/znamen;
-        System.out.println("x="+x3+"; "+"y="+y);
+        x=(float)1.38;
+        y.add(singleValue(a, b, x));
         
-        float x4 = (float)3.21;
-        chisl = (float) Math.log((Math.pow(b,2))-(Math.pow(x4,2)));
-        znamen = (float)Math.pow((Math.abs(Math.pow(x4,2))-(Math.pow(a,2))),(1/3.0));
-        y = chisl/znamen;
-        System.out.println("x="+x4+"; "+"y="+y);
+        x=(float)2.38;
+        y.add(singleValue(a, b, x));
         
-        float x5 = (float)0.68;
-        chisl = (float) Math.log((Math.pow(b,2))-(Math.pow(x5,2)));
-        znamen = (float)Math.pow((Math.abs(Math.pow(x5,2))-(Math.pow(a,2))),(1/3.0));
-        y = chisl/znamen;
-        System.out.println("x="+x5+"; "+"y="+y);
+        x=(float)3.21;
+        y.add(singleValue(a, b, x));
         
+        x=(float)0.68;
+        y.add(singleValue(a, b, x));
+        return y;
+    }
+      
+      //    public void taskB(float a, float b, float[] X) {
+////        float a = 2f ;
+////        float b = 4.1f;
+////        float x = (float)1.24;
+//        System.out.println("taskB");
+//        for (float x:X){
+//            float result = singleValue(a,b,x);
+//            System.out.println("x="+x+"; "+"y="+result);
+       
+
+//    }
+    
+    
+    
+    public ArrayList<Float> taskA(float a,float b,float xn, float xk, float dx) {
+    ArrayList<Float> y= new ArrayList<Float>();
+
+
+//        float xn = (float)0.77f;
+//        float xk = (float)1.77f;
+//        float dx = (float)0.2f;
+//        System.out.println("taskA");
+int i= 0;
+
+        for(float x = xn;x<=xk;x=x+dx){
+y.add(singleValue((int) a, b, x));
+
+        System.out.println("x="+x+"; "+"y="+y.get(i));
+i++;
+
         }
+return y;
+
+    }
+           
     public static void main (String[] args) {
         Calc my = new Calc ();
-        my.taskB();
+        ArrayList<Float> res=my.taskB();
+        for(int i=0;i<res.size(); i++){
+            System.out.println("result >"+res.get(i));
+        }
+        ArrayList<Float> res1=my.taskA(2,4.1f,0.77f,1.77f,0.2f);
+for (int i=0; i<res1.size(); i++){
+System.out.println ("result >" + res1.get(i));
+    
+       }
     }
-
-    ArrayList<Float> taskB(double d, float f, float f0, float f1, float f2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    ArrayList<Float> taskA(int i, float f, float f0, float f1, float f2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-        
-    }
+}
