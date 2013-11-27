fad7a95d0e6c9a23c493b840db886df563c24984
Filippova_I_V <irishka_23-94@mail.ru>
2013-11-27 20:47:38 +0400

    Barbie

---
+++ MyProject/src/Barbara.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author метро
+ */
+public class Barbara extends Barbie {
+    
+}
