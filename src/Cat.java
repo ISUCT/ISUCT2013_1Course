bcc067cb561ad7d189138d18dd2315aadae2ec48
KDaria <dasha9003@yandex.ru>
2013-12-01 19:46:02 +0400

    Cat

---
+++ src/Cat.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Stud_17
+ */
+public class Cat extends Animals{
+    
+}
