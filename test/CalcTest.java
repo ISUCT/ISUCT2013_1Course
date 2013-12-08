39119d7d6372918431b6660ec59cb28026d156a0
Ermakova <nadegda.ermakova@mail.ru>
2013-12-08 18:35:03 +0400

    CalcTest

--- test/CalcTest.java
+++ test/CalcTest.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package test.src;
+package test;
 
 import java.util.ArrayList;
 import junit.framework.TestCase;
