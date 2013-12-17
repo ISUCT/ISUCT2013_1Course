608827e793d2c4edbf4dc78b83a0fbf3f886cb35
Алексей Власов <270696swag@gmail.com>
2013-12-17 10:42:44 -0800

    fix

--- DotCom/DotCom.java
+++ DotCom/DotCom.java
@@ -1,22 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package dotComApp;
 
-package javaapplication1;
+import java.util.*;
 
-/**
- *
- * @author Ольга
- */
-public class JavaApplication1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class DotCom {
+    
+    private ArrayList<String> locationCells;
+    private String name;
+    public void setLocationCells (ArrayList<String> loc){
+        locationCells = loc;
+        
     }
     
-}
+    public void setName(String n) {
+        name = n;
+        
+    }
+    public String checkYourself (String userInput) {
+        
+        String result = "Мимо";
+        int index = locationCells.indexOf (userInput);
+        
+        if (index >= 0) {
+            locationCells.remove(index);
+            
+            if (locationCells.isEmpty()) {
+                result = "Потопил";
+                System.out.println("Ой! Вы потопили" + name +"  : ( ");
+                
+            } else {
+                result ="Попал";
+            }
+        }
+        return result;
+        
+            }
+        }
