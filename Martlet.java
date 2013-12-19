7dddcad2bd6d6939828e9462c6cb63e89dee6101
Your Name <you@example.com>
2013-12-19 20:11:12 +0300

    Dobavlen Martlet

---
+++ Martlet.java
@@ -0,0 +1,18 @@
+/*
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+*/
+package newpackage;
+
+/**
+*
+* @author Stud_17
+*/
+public class Martlet extends Birds{
+    
+    @Override
+    public void makeNoize(){
+        System.out.println("Martlet Noize");
+    }
+    
+}
