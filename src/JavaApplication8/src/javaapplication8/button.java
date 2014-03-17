fabcc160ca9c489585a196797c8bce8f8d581d13
KDaria <dasha9003@yandex.ru>
2014-03-17 22:33:50 +0400

    button

---
+++ src/JavaApplication8/src/javaapplication8/button.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author stud_5
+ */
+class button {
+
+    static void setText(String ive_been_clicked) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+}
