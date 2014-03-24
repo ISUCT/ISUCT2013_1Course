055c954b5723ab1931d007bf574d2305ef95868f
GoldSky37 <1-2-93@bk.ru>
2014-03-24 11:32:36 -0700

    2 semestr.

---
+++ Simpleguil2.java
@@ -0,0 +1,25 @@
+package simpleguil;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Simpleguil2 implements ActionListener {
+JButton button;
+    
+public static void main (String[]args) {
+Simpleguil2 gui = new Simpleguil2();
+gui.go();
+    }
+public void go() {
+JFrame frame = new JFrame();
+button = new JButton("Нажмите на меня");
+        
+button.addActionListener(this);
+frame.getContentPane().add(button);
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.setSize(300,300);
+frame.setVisible(true);
+    }
+public void actionPerformed(ActionEvent event){
+button.setText("I've been clicked!");
+    }
+}
