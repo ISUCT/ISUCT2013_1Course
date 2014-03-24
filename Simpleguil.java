a279a98539be5560d81c7cb4fbd81e9ede3025cb
GoldSky37 <1-2-93@bk.ru>
2014-03-24 11:29:32 -0700

    2 semestr.

---
+++ Simpleguil.java
@@ -0,0 +1,13 @@
+package simpleguil;
+import javax.swing.*;
+
+public class Simpleguil {
+public static void main(String[] args) {
+JFrame frame = new JFrame ();
+JButton button = new JButton ("click me");
+frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+frame.getContentPane().add(button);
+frame.setSize(250,250);
+frame.setVisible(true);
+    }
+}
