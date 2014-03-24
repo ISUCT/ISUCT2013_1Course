4ca6eb2b3da72e209fc5edb8d8d6fcc55043383c
GoldSky37 <1-2-93@bk.ru>
2014-03-24 11:33:23 -0700

    2 semestr.

--- Simpleguil.java
+++ Simpleguil.java
@@ -5,7 +5,7 @@ public class Simpleguil {
 public static void main(String[] args) {
 JFrame frame = new JFrame ();
 JButton button = new JButton ("click me");
-frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+frame.setDefaultCloseOperation (JFrame.Exit_on_close);
 frame.getContentPane().add(button);
 frame.setSize(250,250);
 frame.setVisible(true);
