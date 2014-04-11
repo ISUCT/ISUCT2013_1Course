116d13499e65e71d48de8ba2256f95b162a34c26
NastasiaBusova <nastasia_2295@mail.ru>
2014-04-11 10:37:54 +0400

    Dobavlenie files

---
+++ src/myproject/MyDrawPanel.java
@@ -0,0 +1,17 @@
+package myproject;
+import javax.swing.*;
+import java.awt.*;
+/**
+ *
+ * @author stud_5
+ */
+class MyDrawPanel extends JPanel{
+    public void paintComponent(Graphics g){
+        g.setColor(Color.orange);
+        g.fillRect(20, 50, 100, 100);
+    }
+public static void main(String[] args) {
+MyDrawPanel my = new MyDrawPanel();
+
+}
+}
