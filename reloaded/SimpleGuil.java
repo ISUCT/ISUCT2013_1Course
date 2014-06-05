369a29750ae98cd5165b3fcf02452f9399653295
stas147 <gorynov_stas@mail.ru>
2014-06-05 17:59:10 +0400

    reloaded

---
+++ reloaded/SimpleGuil.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+import javax.swing.*;
+
+
+
+/**
+ *
+ * @author stud_5
+ */
+public class SimpleGuil {
+     public static void main (String[] args) {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("click me");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame.getContentPane().add(button);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+        
+     }
+        public void changeIt() {
+            button.setText("I've been clicked!");
+            
+        }
+        
+}
